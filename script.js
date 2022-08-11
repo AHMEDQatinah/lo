3a6d27582eb5f4248fcff971806b0fdec9394361
AHMEDQatinah <106144237+AHMEDQatinah@users.noreply.github.com>
2022-08-11 10:06:59 +0300

    Initial-commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var div1=document.getElementById("one");
+var div2=document.getElementById("two");
+var div3=document.getElementById("three");
+var div4=document.getElementById("four");
+
+div1.addEventListener("click",function(){
+    document.phoneImg.src="img/red 2.png";
+});
+
+
+div2.addEventListener("click",function(){
+    document.phoneImg.src="img/NAPSS00314BL.jpg";
+});
+
+
+div3.addEventListener("click",function(){
+    document.phoneImg.src="img/iphone-13-pink-select-2021.jfif";
+});
+
+
+div4.addEventListener("click",function(){
+    document.phoneImg.src="img/green.webp";
+});
